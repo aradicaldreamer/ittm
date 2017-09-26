3e2701553128af5ace6364dff5dfc2492fa4a91e
tommygraven <tommygraven@me.com>
2017-09-26 13:05:13 +0100

    Shaving scripts working
    -Blade following mouse cursor
    -Shave blade working properly
    (destroying creme balls)

---
+++ Shaving game/Assets/DestroyCreme.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCreme : MonoBehaviour {
+	[SerializeField] GameObject cremeBallPrefab;
+	bool isShaving;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckIfShaving();
+	}
+
+	void CheckIfShaving()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			isShaving = true;
+		}
+
+		else isShaving = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == cremeBallPrefab.tag && isShaving)
+		{
+			print("destroy circle");
+			Destroy(col.gameObject);
+		}
+	}
+}
